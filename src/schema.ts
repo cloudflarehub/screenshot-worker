dcedd4679ee9c56383873bca955357c6e636adf1
guangzhengli <iguangzhengli@gmail.com>
2024-07-13 22:21:21 +0800

    fix: change zod validate number

--- src/schema.ts
+++ src/schema.ts
@@ -3,10 +3,10 @@ import {z} from "zod";
 export const ScreenshotOptionsSchema = z.object({
   url: z.string().url(),
   device: z.string().optional().default('desktop'),
-  width: z.number().int().min(1).optional(),
-  height: z.number().int().min(1).optional(),
+  width: z.coerce.number().int().min(1).optional(),
+  height: z.coerce.number().int().min(1).optional(),
   fullPage: z.string().optional().default('false'),
-  quality: z.number().optional().default(80),
+  quality: z.coerce.number().optional().default(80),
   isBlockAds:  z.string().optional().default('false'),
 });
 
